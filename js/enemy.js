2e11b80e74450a26c1cb406acd32245459c34b76
DelioCoder <davideveriwhere@gmail.com>
2026-06-16 15:18:21 -0500

    Added: new enemies

--- js/enemy.js
+++ js/enemy.js
@@ -1,17 +1,35 @@
+const ENEMY_ESCORT_OFFSET_Y = 220;
+const ENEMY_ESCORT_OFFSET_X = 80;
+
 function createEnemy() {
-    enemy = game.add.sprite(1400, game.height / 2, 'enemyBoat');
+    enemy = game.add.sprite(BATTLE_POSITIONS.enemyX, BATTLE_POSITIONS.enemyY, 'enemyBoat');
     enemy.anchor.setTo(0.5);
     enemy.frame = 1;
     enemy.scale.x = 1;
     game.physics.arcade.enable(enemy);
+
+    // Botes refuerzo
+    enemyTop = game.add.sprite(BATTLE_POSITIONS.enemyX + ENEMY_ESCORT_OFFSET_X, BATTLE_POSITIONS.enemyY - ENEMY_ESCORT_OFFSET_Y, 'enemyBoat');
+    enemyTop.anchor.setTo(0.5);
+    enemyTop.frame = 1;
+    enemyTop.scale.x = 1;
+    game.physics.arcade.enable(enemyTop);
+
+    enemyBottom = game.add.sprite(BATTLE_POSITIONS.enemyX + ENEMY_ESCORT_OFFSET_X, BATTLE_POSITIONS.enemyY + ENEMY_ESCORT_OFFSET_Y, 'enemyBoat');
+    enemyBottom.anchor.setTo(0.5);
+    enemyBottom.frame = 1;
+    enemyBottom.scale.x = 1;
+    game.physics.arcade.enable(enemyBottom);
 }
 
 function checkCollision() {
-    game.physics.arcade.overlap(bullet, enemy, () => {
-        bullet.exists = false;
-        bulletActive = false;
-        currentHP -= 20;
-        updateGrauFace();
-        alert('¡Colisión! El Huáscar alcanzó al barco enemigo.');
+    [enemy, enemyTop, enemyBottom].forEach((target) => {
+        game.physics.arcade.overlap(bullet, target, () => {
+            bullet.exists = false;
+            bulletActive = false;
+            currentHP -= 20;
+            updateGrauFace();
+            alert('¡Colisión! El Huáscar alcanzó al barco enemigo.');
+        });
     });
 }
